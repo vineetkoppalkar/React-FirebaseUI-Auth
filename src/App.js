7ef1ffd1837d81d7bbd81e27f2ee31add89a41fe
vineetkoppalkar <koppalkarvineet@gmail.com>
2019-07-23 14:09:29 -0400

    Setup Firebase UI Auth

--- src/App.js
+++ src/App.js
@@ -1,42 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-import firebase from './firebase';
+import SignInScreen from './SignInScreen';
 
 class App extends React.Component  {
   componentDidMount() {
-    let testData = {
-      title: "Test Title",
-      value: "Test value"
-    }
-    firebase.firestore().collection("testCollection").add(testData)
-    .then((docRef) => {
-      console.log("Added document to collection");
-      console.log({docRef});
-    })
-    .catch((error) => {
-      console.error("Error adding document: ", error);
-    });
+    // This is used to test firestore
+    // let testData = {
+    //   title: "Test Title",
+    //   value: "Test value"
+    // }
+    // firebase.firestore().collection("testCollection").add(testData)
+    // .then((docRef) => {
+    //   console.log("Added document to collection");
+    //   console.log({docRef});
+    // })
+    // .catch((error) => {
+    //   console.error("Error adding document: ", error);
+    // });
   }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <SignInScreen />
     );
   }
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,13 @@
 import * as firebase from 'firebase';
 
-const settings = {};
-
 const config = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  apiKey: "AIzaSyBZbxj8C3XSMT6PzQ5fQa0NvJDu8LHrsTc",
+  authDomain: "fir-ui-auth-76c12.firebaseapp.com",
+  databaseURL: "https://fir-ui-auth-76c12.firebaseio.com",
+  projectId: "fir-ui-auth-76c12",
+  storageBucket: "",
+  messagingSenderId: "1052984360912",
+  appId: "1:1052984360912:web:a227db2a5201155e"
 }
 firebase.initializeApp(config);
-firebase.firestore().settings(settings);
 export default firebase;
